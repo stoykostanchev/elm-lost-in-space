9dbfd61e55c61755fadd1e5da702e8e439d0f723
Stoyko Stanchev <stoyko.stanchev@gomedia.io>
2019-11-25 20:53:57 +0000

    Updates

---
+++ precache-manifest.f85ff985e8aa0cb589b829210df67c0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-lost-in-space/favicon.ico"
+  },
+  {
+    "revision": "6151c4cc9eeca2e3aa80dc1da8d6d48d",
+    "url": "/elm-lost-in-space/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-lost-in-space/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-lost-in-space/manifest.json"
+  },
+  {
+    "revision": "3296516fc475e45c7475",
+    "url": "/elm-lost-in-space/static/css/main.7c47601a.chunk.css"
+  },
+  {
+    "revision": "3296516fc475e45c7475",
+    "url": "/elm-lost-in-space/static/js/main.3296516f.chunk.js"
+  },
+  {
+    "revision": "8ad43a4815bed44e7077",
+    "url": "/elm-lost-in-space/static/js/runtime~main.8ad43a48.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/elm-lost-in-space/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
